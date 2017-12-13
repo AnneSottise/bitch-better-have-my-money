7e72a47a85adfe9ec29e2278351774bae08456ed
Anne-Sophie Rouaux <annesophie.rouaux@gmail.com>
2017-12-13 22:23:00 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      	My first React component \o\ \o/ /o/
+      </div>
+    );
+  }
+}
+
+export default App;
